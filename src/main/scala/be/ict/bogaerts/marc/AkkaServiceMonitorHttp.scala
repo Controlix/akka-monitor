b91ee84a066bfd21682ade061a92dec32e909948
Marc Bogaerts <marcbogaerts@telenet.be>
2018-06-04 20:26:41 +0200

    Extract service

--- src/main/scala/be/ict/bogaerts/marc/AkkaServiceMonitorHttp.scala
+++ src/main/scala/be/ict/bogaerts/marc/AkkaServiceMonitorHttp.scala
@@ -9,10 +9,7 @@ import akka.http.scaladsl.server.directives.DebuggingDirectives
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.DefaultJsonProtocol
 
-case class StartMonitoring(method: String, url: String)
-case class StopMonitoring(id: String)
-case class MonitoringStarted(id: String)
-case class MonitoringStopped(id: String)
+import AkkaServiceMonitorService._
 
 trait ServiceMonitorJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val startMonitoringFormat = jsonFormat2(StartMonitoring)
@@ -23,45 +20,26 @@ trait ServiceMonitorJsonSupport extends SprayJsonSupport with DefaultJsonProtoco
 
 object AkkaServiceMonitorHttp extends HttpApp with ServiceMonitorJsonSupport {
 
-  import ServiceMonitor._
+  private lazy val system = systemReference.get
+  private lazy val log = system.log
+  private lazy val service = AkkaServiceMonitorService(system)
 
   val routes =
     pathPrefix("monitor") {
-      get {
-        log.info("Start monitoring google for 2 seconds ...")
-        monitorGoogle
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>google was monitored</h1>"))
-      } ~ post {
+      post {
         path("start") {
           entity(as[StartMonitoring]) { startMonitoring =>
             log.info("got message {}", startMonitoring)
-            val serviceMonitor = system.actorOf(ServiceMonitor.props(startMonitoring.method.toUpperCase(), startMonitoring.url))
-            serviceMonitor ! Start
-            complete(MonitoringStarted(serviceMonitor.path.name))
+            complete(service.startMonitor(startMonitoring))
           }
         }
       } ~ post {
         path("stop") {
           entity(as[StopMonitoring]) { stopMonitoring =>
             log.info("got message {}", stopMonitoring)
-            val serviceMonitor = system.actorSelection(s"user/${stopMonitoring.id}")
-            serviceMonitor ! Stop
-            complete(MonitoringStopped(stopMonitoring.id))
+            complete(service.stopMonitor(stopMonitoring))
           }
         }
       }
     }
-
-  private lazy val system = systemReference.get
-  private lazy val log = system.log
-
-  private lazy val monitorGoogle = {
-    val serviceMonitor = system.actorOf(ServiceMonitor.props("GET", "http://google.com?q=akka"))
-
-    serviceMonitor ! Start
-
-    TimeUnit.SECONDS.sleep(2L)
-
-    serviceMonitor ! Stop
-  }
 }
---
+++ src/main/scala/be/ict/bogaerts/marc/AkkaServiceMonitorService.scala
@@ -0,0 +1,35 @@
+package be.ict.bogaerts.marc
+
+import akka.actor.ActorSystem
+
+object AkkaServiceMonitorService {
+  case class StartMonitoring(method: String, url: String)
+  case class StopMonitoring(id: String)
+
+  case class MonitoringStarted(id: String)
+  case class MonitoringStopped(id: String)
+
+  def apply(system: ActorSystem): AkkaServiceMonitorService = {
+    new AkkaServiceMonitorService(system)
+  }
+}
+
+class AkkaServiceMonitorService(val system: ActorSystem) {
+
+  import AkkaServiceMonitorService._
+  import ServiceMonitor._
+
+  def startMonitor(startMonitoring: StartMonitoring): MonitoringStarted = {
+    val serviceMonitor = system.actorOf(ServiceMonitor.props(startMonitoring.method.toUpperCase(), startMonitoring.url))
+    serviceMonitor ! Start
+    
+    MonitoringStarted(serviceMonitor.path.name)
+  }
+
+  def stopMonitor(stopMonitoring: StopMonitoring): MonitoringStopped = {
+    val serviceMonitor = system.actorSelection(s"user/${stopMonitoring.id}")
+    serviceMonitor ! Stop
+    
+    MonitoringStopped(stopMonitoring.id)
+  }
+}
